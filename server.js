f65c913fefe46b69c822d6a7c22ab34b14f5e9a4
aajayk <aajayk786@gmail.com>
2020-10-17 15:09:49 +0530

    JSON res

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 
 app.get('/getLocation',(req, res)=>{
     console.log("req headers:"+JSON.stringify(req.headers))
-    console.log(req.headers['x-forwarded-for']||'105.41.38.132')
     var ip = req.headers['x-forwarded-for']||'104.41.38.132';
     callIPStack(ip,(result)=>{
-        res.send(result)
+        //var jsonRes=JSON.parse(result)
+        res.json(result)
     })
    // res.send('Ok')
 })
@@ -20,8 +20,23 @@ app.listen(process.env.PORT || 3000)
 function callIPStack(ip,callback){
     var user_ip=ip
     var access_key=process.env.IP_APIKEY
-    request(`http://api.ipstack.com/${user_ip}?access_key=${access_key}&format=1`,(req,res)=>{
-        console.log("IP response :"+res)
-        callback(res)
+
+    const options = {
+        url: `http://api.ipstack.com/${user_ip}?access_key=${access_key}&format=1&output=json&language=en`,
+        headers: {
+          'content-type': 'application/json'
+        }
+      };
+
+    request(options ,(req,res)=>{
+        
+        console.log(res.statusCode)
+        if(res.statusCode){
+            console.log(JSON.parse(res.body))
+
+            callback(JSON.parse(res.body))
+        }
+        else
+        callback(new Error('IP Call failed'))
     })
 }
